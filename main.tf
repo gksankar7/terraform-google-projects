f30092980a0a24ed6f687720408d88487602eaf8
gksankar7 <57542387+gksankar7@users.noreply.github.com>
2021-02-12 15:39:36 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+locals {
+  prefix       = var.prefix == "" ? "" : "${var.prefix}-"
+  projects_list = [for name in var.names : google_projects.projects[name]]
+
+  name_role_pairs = setproduct(var.names, var.folder_admin_roles)
+  folder_admin_roles_map_data = zipmap(
+    [for pair in local.name_role_pairs : "${pair[0]}-${pair[1]}"],
+    [for pair in local.name_role_pairs : {
+      name = pair[0]
+      role = pair[1]
+    }]
+  )
+}
+
+resource "google_projects" "projects" {
+  for_each = toset(var.names)
+
+  display_name = "${local.prefix}${each.value}"
+  parent       = var.parent
+}
